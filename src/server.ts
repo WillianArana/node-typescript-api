b477bf5959c7329b00ff7f61a818419a17989447
Willian Arana <willcruzarana@gmail.com>
2022-03-25 00:02:46 -0300

    C04

--- src/server.ts
+++ src/server.ts
@@ -5,15 +5,18 @@ import bodyParser from 'body-parser';
 import { Application } from 'express';
 
 import { ForecastController } from './controllers/forecast';
+import * as database from '@src/database';
+import { BeachesController } from './controllers/beaches';
 
 export class SetupServer extends Server {
-  constructor(private _port = 3000) {
+  constructor(protected readonly _port = 3000) {
     super();
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
+    await this.databaseSetup();
   }
 
   private setupExpress(): void {
@@ -22,10 +25,25 @@ export class SetupServer extends Server {
 
   private setupControllers(): void {
     const forecastController = new ForecastController();
-    this.addControllers([forecastController]);
+    const beachesController = new BeachesController();
+    this.addControllers([forecastController, beachesController]);
+  }
+
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
   }
 
   public getApp(): Application {
     return this.app;
+  }
+
+  public start(): void {
+    this.app.listen(this._port, () => {
+      console.info('Server listening of port:', this._port);
+    });
   }
 }
---
+++ test/functional/beaches.test.ts
@@ -0,0 +1,55 @@
+import { Beach } from '@src/models/beach';
+
+describe('Beache functional tests', () => {
+  beforeAll(async () => {
+    await Beach.deleteMany({});
+  });
+  describe('When createing a new beach', () => {
+    it('should create a beach with success', async () => {
+      const newBeach = {
+        lat: -33.792726,
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      };
+
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+      expect(response.status).toBe(201);
+      //Object containing matches the keys and values, even if includes other keys such as id.
+      expect(response.body).toEqual(expect.objectContaining(newBeach));
+    });
+
+    it('should return 422 when there is a validation error', async () => {
+      const newBeach = {
+        lat: 'invalid_string',
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      };
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error:
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) at path "lat"',
+      });
+    });
+
+    it('should return 500 when there is any error other than validation error', async () => {
+      jest.spyOn(Beach.prototype, 'save').mockRejectedValueOnce('fail to create beach');
+
+      const newBeach = {
+        lat: 'invalid_string',
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      };
+
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+      expect(response.status).toBe(500);
+      expect(response.body).toEqual({
+        error: 'Internal Server Error',
+      });
+    });
+  });
+});
